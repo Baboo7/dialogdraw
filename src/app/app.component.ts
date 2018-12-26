3ff63d178760fdea7127b392308084778e9176d2
Baboo7 <dbsharp404@gmail.com>
2018-12-26 01:21:22 +0100

    Draw dialog flow

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,42 @@
-import { Component } from '@angular/core';
+import * as shape from 'd3-shape';
+
+import { Component, OnInit } from '@angular/core';
+import { DialogflowService } from './services/dialogflow.service';
+import { GraphService } from './services/graph.service';
+import { Intent } from './types/intent.interface';
+import { Node, Link } from './types/graph.interface';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'dialogdraw';
+export class AppComponent implements OnInit {
+
+  curve: any = shape.curveBundle;
+  view: any[] = [1920, 1080];
+  autoZoom = false;
+  panOnZoom = true;
+  enableZoom = true;
+  autoCenter = false;
+
+  nodes: Node[] = [];
+  links: Link[] = [];
+  intents: Intent[] = [];
+
+  constructor(
+    private dialogflowService: DialogflowService,
+    private graphService: GraphService,
+  ) { }
+
+  async ngOnInit(): Promise<void> {
+    this.intents = await this.dialogflowService.fetchIntents();
+    const { nodes, links } = this.graphService.getGraphFromIntents(this.intents);
+    this.nodes = nodes;
+    this.links = links;
+  }
+
+  select(event): void {
+    console.log(event);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { NgxChartsModule } from '@swimlane/ngx-charts';
+import { NgxGraphModule } from '@swimlane/ngx-graph';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -10,7 +14,11 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgxChartsModule,
+    NgxGraphModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
